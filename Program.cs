b52d2e4cb5afe1554f5d279684a774f681812536
saki <>
2025-12-12 16:41:43 +0900

    Cardクラスの作成

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿public class Card
+{
+    public string Rank {get;}   // A, 2, 3, ..., K
+    public int Value {get;}     // 点数
+
+    public Card(string rank)
+    {
+        Rank = rank;
+        Value = CalculateValue(rank);
+    }
+
+    private int CalculateValue(string rank)
+    {
+        if (rank == "A") return 1;       // Aは1点
+        if (rank == "J" || rank == "Q" || rank == "K") return 10;
+        return int.Parse(rank);          // 2〜10はそのまま数値
+    }
+
+    public override string ToString()
+    {
+        return "{Rank}";
+    }
+}
