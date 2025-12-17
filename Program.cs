8beb645c9c0307b06ea3ed5248a12030fed7ac0a
saki <>
2025-12-17 13:19:50 +0900

    ディーラーのターンをテスト

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@
     static void Main(string[] args)
     {
         GameManager gameManager = new GameManager();
+        // ゲーム開始
         gameManager.StartGame();
+        // ディーラーのターンを実行
+        gameManager.DealerTurn();
     }
 }
