11301a46bc1005fd44ac334e432fc11333ef930e
saki <>
2025-12-17 09:18:52 +0900

    PlayerBaseクラスを作成

---
+++ PlayerBase.cs
@@ -0,0 +1,33 @@
+/// <summary>
+/// プレイヤー／ディーラー共通の動きをもつクラス
+/// </summary>
+public class PlayerBase
+{
+    /// <summary>
+    /// プレイヤー／ディーラーの手札
+    /// </summary>
+    public List<Card> Hand {get;} = new List<Card>();
+
+    /// <summary>
+    /// 手札にカードを追加
+    /// </summary>
+    /// <param name="card">追加するカード</param>
+    public void AddCard(Card card)
+    {
+        Hand.Add(card);
+    }
+
+    /// <summary>
+    /// 手札の合計点を計算する
+    /// </summary>
+    /// <returns>手札の合計点</returns>
+    public int CalculateHandValue()
+    {
+        int total = 0;
+        foreach (Card card in Hand)
+        {
+            total += card.Value;
+        }
+        return total;
+    }
+}
